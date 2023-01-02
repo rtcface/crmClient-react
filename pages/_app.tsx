a4130adfcd366ab7658ecd1ab9de7a02c27fa23b
Ramiro Tepehua <contactocsip@gmail.com>
2023-01-01 21:43:21 -0600

    client react first ok

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../config/apollo'
+ 
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+          <ApolloProvider client={ client }>
+            <Component {...pageProps} />
+          </ApolloProvider>
+            )
 }
